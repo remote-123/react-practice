166a86fcd5272d90b6afbaa826213c3dd28778f4
DeFiGuy <governancesale@protonmail.com>
2020-09-18 12:14:50 -0400

    Upload build folder

---
+++ build/precache-manifest.e5fcfb54868353b27b851e88b95fde1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ab5b45d7f108b44a3c8d6f0876f11e",
+    "url": "/react-practice/index.html"
+  },
+  {
+    "revision": "a96f3cb59c6cca3a8a5c",
+    "url": "/react-practice/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "02406d79ef92c3e26977",
+    "url": "/react-practice/static/js/2.d630df8e.chunk.js"
+  },
+  {
+    "revision": "d32e84ffdfe232e841f483868020a5c2",
+    "url": "/react-practice/static/js/2.d630df8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a96f3cb59c6cca3a8a5c",
+    "url": "/react-practice/static/js/main.44f7b22a.chunk.js"
+  },
+  {
+    "revision": "8ea98f572727ec0feed2",
+    "url": "/react-practice/static/js/runtime-main.df42490b.js"
+  }
+]);
